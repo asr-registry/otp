3d0178d19525fd377ecb74925de352c8123d0c14
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-30 14:49:59 +0430

    repositories inmemory otp settings

---
+++ src/main/java/af/asr/otp/otp/data/repository/InMemoryOTPSettingsRepository.java
@@ -0,0 +1,32 @@
+package af.asr.otp.otp.data.repository;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryOTPSettingsRepository implements OTPSettingsRepository {
+
+    private Map<String, OTPSetting> otpSettingMap;
+
+    public InMemoryOTPSettingsRepository() {
+        otpSettingMap = new HashMap<>();
+        OTPSetting smsSetting = new OTPSetting();
+        smsSetting.setId("sms");
+        smsSetting.setAccountId(null);
+        smsSetting.setMessageTemplate("Code: ${otp}");
+        smsSetting.setName("SMS sender");
+        smsSetting.setOtpLength(6);
+        smsSetting.setUseDigits(true);
+        smsSetting.setUseLetters(false);
+        smsSetting.setTtlMinutes(3);
+        smsSetting.setSender("dummyOtpSender");
+
+        otpSettingMap.put(smsSetting.getId(), smsSetting);
+    }
+
+
+    @Override
+    public OTPSetting getSetting(String settingId) {
+        return otpSettingMap.get(settingId);
+    }
+}
