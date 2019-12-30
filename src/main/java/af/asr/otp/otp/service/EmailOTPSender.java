13752326771202c3e9d5b9a06a02088b4e486d39
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-30 14:58:12 +0430

    otp email sender

---
+++ src/main/java/af/asr/otp/otp/service/EmailOTPSender.java
@@ -0,0 +1,34 @@
+package af.asr.otp.otp.service;
+
+import af.asr.otp.otp.data.model.OtpSetting;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+import java.util.Map;
+
+@Log4j2
+public class EmailOTPSender implements OTPSender {
+
+    private final MailSender mailSender;
+
+    public EmailOTPSender(MailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Override
+    public void sendOTP(OtpSetting otpSetting, String otp, String destination, Map<String, String> properties) {
+        String messageStr = this.createMessage(otpSetting, otp, destination, properties);
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(destination);
+        msg.setText(messageStr);
+        msg.setSubject(otpSetting.getMessageTitle());
+        try{
+            this.mailSender.send(msg);
+        }
+        catch (MailException ex) {
+            log.error(ex);
+        }
+    }
+}
