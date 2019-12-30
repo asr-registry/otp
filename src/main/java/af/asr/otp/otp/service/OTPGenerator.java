b958c0028a91956c73e7df295db98d097710c7da
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-30 14:57:22 +0430

    otp generator

---
+++ src/main/java/af/asr/otp/otp/service/OTPGenerator.java
@@ -0,0 +1,21 @@
+package af.asr.otp.otp.service;
+
+import af.asr.otp.otp.data.model.OtpSetting;
+import af.asr.otp.otp.data.model.SentOTP;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class OTPGenerator {
+
+    public SentOTP generateSentOTP(OtpSetting otpSetting) {
+        String OTP = RandomStringUtils.random(otpSetting.getOtpLength(), otpSetting.isUseLetters(), otpSetting.isUseDigits());
+        SentOTP sentOTP = new SentOTP();
+        sentOTP.setOperationId(UUID.randomUUID().toString());
+        sentOTP.setExpireTime(System.currentTimeMillis() + otpSetting.getTtlMinutes() * 60 * 1000);
+        sentOTP.setOTP(OTP);
+        return sentOTP;
+    }
+}
