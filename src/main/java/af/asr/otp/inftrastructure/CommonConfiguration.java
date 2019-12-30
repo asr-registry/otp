c6aa5f30d96c6617830ce47092606b00090e7669
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-30 14:36:06 +0430

    common config

---
+++ src/main/java/af/asr/otp/inftrastructure/CommonConfiguration.java
@@ -0,0 +1,17 @@
+package af.asr.otp.inftrastructure;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CommonConfiguration {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return objectMapper;
+    }
+}
